cc0f8b9082f4f834a14d3273406e76c665e544e5
darrylgit <darrylsmithup@gmail.com>
2020-02-07 15:29:57 -0500

    Install and implement React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
