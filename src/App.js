00c1d1902806203df46e189873f4e0d95e6cfee2
darrylgit <darrylsmithup@gmail.com>
2020-02-07 14:52:09 -0500

    Render menu items with directory component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./hompage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return <HomePage />;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import sections from "./directory.data";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => {
+          return (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Directory;
