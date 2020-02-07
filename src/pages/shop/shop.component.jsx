8fb96f410af29bacf4e20b1fe028c65041eb1000
darrylgit <darrylsmithup@gmail.com>
2020-02-07 17:42:16 -0500

    Create collection preview component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import collections from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...collectionProps }) => (
+          <CollectionPreview key={id} {...collectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
