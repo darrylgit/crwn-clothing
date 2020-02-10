d339ff19758d502e08cfd4fab0bdd4d05c3f67c5
darrylgit <darrylsmithup@gmail.com>
2020-02-10 13:33:35 -0500

    Integrate Firebase into header and sign-in

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const header = () => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+const header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         Shop
       </Link>
-      <Link className="option" to="/contact">
+      <Link className='option' to='/contact'>
         Contact
       </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          Sign In
+        </Link>
+      )}
     </div>
   </div>
 );
