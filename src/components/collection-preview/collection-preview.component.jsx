fb361ecf6bf43d241986b9b954a92b0691a6d565
darrylgit <darrylsmithup@gmail.com>
2020-02-11 20:03:35 -0500

    Add items to cart with Redux

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,16 +4,16 @@ import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
-	<div className="collection-preview">
-		<h1 className="title">{title}</h1>
-		<div className="preview">
-			{items
-				.filter((item, index) => index < 4)
-				.map(({ id, ...itemProps }) => (
-					<CollectionItem key={id} {...itemProps} />
-				))}
-		</div>
-	</div>
+  <div className='collection-preview'>
+    <h1 className='title'>{title}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
 );
 
 export default CollectionPreview;
