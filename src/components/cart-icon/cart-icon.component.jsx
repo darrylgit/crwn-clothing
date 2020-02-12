284d044ed99f7a48550c6e141d9c7c9f04d696cf
darrylgit <darrylsmithup@gmail.com>
2020-02-11 21:31:11 -0500

    Update item count when user adds to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,11 +7,15 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
-export default connect(null, { toggleCartHidden })(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, item) => acc + item.quantity, 0)
+});
+
+export default connect(mapStateToProps, { toggleCartHidden })(CartIcon);
