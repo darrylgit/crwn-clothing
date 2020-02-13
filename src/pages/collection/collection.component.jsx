63c3cd293d5553bfd72666336b849d2de6a6c2c1
darrylgit <darrylsmithup@gmail.com>
2020-02-13 18:51:47 -0500

    Update name of category page to collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection-page'>
+    <h2>collection PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
