95db54f0325b2f92e861cb36b7cdbf9102f447a2
darrylgit <darrylsmithup@gmail.com>
2020-02-14 12:30:19 -0500

    Integrate Stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const pubKey = 'pk_test_yM70W2RZ7Otzi6yLDSuEi74D00lZvwS3SV';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful!');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={pubKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 import {
   selectCartItems,
   selectCartTotal
@@ -11,30 +12,36 @@ import {
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
         <span>Product</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Description</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Quantity</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Price</span>
       </div>
-      <div className='header-block'>
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
     {cartItems.map(item => (
       <CheckoutItem key={item.id} cartItem={item} />
     ))}
-    <div className='total'>
+    <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242-4242-4242-4242
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
