95dbf3e165832e2ae7ae75dd4635540be97f7307
darrylgit <darrylsmithup@gmail.com>
2020-02-14 08:35:57 -0500

    Select collection from URL param

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
-  <div className='collection-page'>
-    <h2>collection PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className="collection-page">
+      <h2>collection PAGE</h2>
+    </div>
+  );
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,12 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
 
 const selectShop = state => state.shop;
 
@@ -6,3 +14,10 @@ export const selectShopCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector([selectShopCollections], collections =>
+    collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
