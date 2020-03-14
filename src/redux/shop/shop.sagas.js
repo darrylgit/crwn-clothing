a58db671e3a72834375882d27e2768d2778a30df
darrylgit <darrylsmithup@gmail.com>
2020-03-14 15:53:27 -0400

    Create saga to clear cart on sign out

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import {
   firestore,
@@ -13,8 +13,6 @@ import {
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
-
   try {
     const collectionRef = firestore.collection('collections');
     const snapshot = yield collectionRef.get();
@@ -33,4 +31,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
